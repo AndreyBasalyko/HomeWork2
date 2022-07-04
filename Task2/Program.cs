7eec1afc6673c6c27c7f134a2cf5f44bd6450619
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 19:31:02 +0300

    Выполнил вторую задачу.

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число.");
+string i = Console.ReadLine();
+string a = i.Substring(2,1);
+if (i.Length < 3)
+{
+   Console.Write("Третьей цифры нет.");
+}
+else
+{
+    Console.Write(a);
+}
