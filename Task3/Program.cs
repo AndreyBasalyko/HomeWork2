63b4f8309679d80fae55786fabb354a36448ce5c
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 20:54:15 +0300

    Выролнил третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите число.");
+int i = Convert.ToInt32(Console.ReadLine()); 
+if (i < 8)
+{
+    if (i > 0) 
+      {
+          if (i == 7)
+          {
+           Console.Write("Выходной");
+          }
+         else
+          {
+            if (i == 6)
+            {
+            Console.Write("Выходной");
+            }
+            else
+            {
+            Console.Write("Будний");
+            }
+            }
+        }
+    else 
+    {
+    Console.Write("не соответствует дням недели.");
+    }
+}     
+else 
+{
+    Console.Write("не соответствует дням недели.");
+}
