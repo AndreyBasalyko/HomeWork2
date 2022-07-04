e77e814c3e2be282512cf7788af33856e74ea41b
Андрей Басалыко <basalykoa.s@gmail.com>
2022-07-04 19:23:45 +0300

    Выполнил первую задачу.

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трехзначное число.");
+string i = Console.ReadLine();
+string a = i.Substring(1,1);
+if (i.Length > 3)
+{
+    Console.Write("Слишком большое число.");
+}
+if (i.Length < 3)
+{
+   Console.Write("Слишком маленькое число.");
+}
+if (i.Length == 3)
+{
+    Console.Write(a);
+}
